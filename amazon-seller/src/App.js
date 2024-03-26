13b54f70a242b096b70406f275a3cb2527ae10d1
ambaqinejad <ambaqinejad@gmail.com>
2024-03-26 12:23:26 +0430

    react props and children

--- amazon-seller/src/App.js
+++ amazon-seller/src/App.js
@@ -1,4 +1,28 @@
 import "./App.css";
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
+const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Stephen King",
+    title: "Fairy Tale",
+    img: img3,
+    id: 3,
+  },
+];
 
 function App() {
   return <BookList />;
@@ -7,24 +31,27 @@ function App() {
 const BookList = () => {
   return (
     <section className="bookList">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book bookInfo={book} key={book.id}>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum sit corrupti quibusdam voluptates saepe nisi exercitationem repudiandae nihil tenetur facere similique non, inventore quos in omnis quaerat quidem itaque vel?</p>
+          <button>Click Me</button>
+        </Book>;
+      })}
     </section>
   );
 };
 
-const Book = () => {
-  const title = "The Women: A Novel";
+const Book = ({ bookInfo, children }) => {
+  console.log(children);
   return (
     <article className="book">
-      <img src="./images/book-img.jpg" alt="The Women: A Novel" />
-      
-      <h2>{title}</h2>
+      <img src={bookInfo.img} alt={bookInfo.title} />
+
+      <h2>{bookInfo.title}</h2>
       <p style={{ color: "#617d98", fontSize: "0.75rem" }}>
-        by Kristin Hannah (Author)
+        {bookInfo.author} (Author)
       </p>
+      {children}
     </article>
   );
 };
